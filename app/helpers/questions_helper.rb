4959b1c61098e50d0f55392ecd890c830aebddea
Anirudh Garg <anirudh@clearstack.io>
2018-05-21 16:22:11 +0530

    all questions ui and tag lists

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,7 +1,11 @@
 module QuestionsHelper
 
   def display_tags(tags)
-    #ToDo: Return comma separated tags from here
-    tags
+    html = ''
+    tags.map(&:name).map do |tag|
+      html << "<span class='badge new blue' data-badge-caption=''>#{tag.upcase}</span>"
+    end
+    html.html_safe
   end
+
 end
