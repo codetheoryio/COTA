915d9647eedbb9421c9598ffbaee5da2638f7017
Balamurugan <balasvn08@gmail.com>
2018-05-30 15:53:43 +0530

    Initial commit for invite candidates

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,8 @@
 class Candidate < ActiveRecord::Base
+  # Validations
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true, format: Devise.email_regexp
+
   has_many :quiz_candidates
   has_many :quizzes, through: :quiz_candidate
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'candidates/invite'
+  post 'candidates/send_invite'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
